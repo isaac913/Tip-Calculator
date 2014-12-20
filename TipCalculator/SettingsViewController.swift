1a5ae08bee6369b1ff8d04459f1da8541ed05010
isaac913 <owner1@Isaacs-MBP-old.att.net>
2014-12-20 12:31:40 -0800

    update NO.2

---
+++ TipCalculator/SettingsViewController.swift
@@ -0,0 +1,49 @@
+//
+//  SettingsViewController.swift
+//  TipCalculator
+//
+//  Created by isaac on 12/18/14.
+//  Copyright (c) 2014 Isaac Zhang. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    var initialSeletedIndex : Int = 0;
+    
+    @IBOutlet weak var defaultTipPercentageControl: UISegmentedControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        defaultTipPercentageControl.selectedSegmentIndex = initialSeletedIndex;
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated);
+        self.navigationController?.navigationBar.barTintColor = UIColor.blackColor();
+        self.navigationController?.navigationBar.tintColor = UIColor.whiteColor();
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onValueChanged(sender: UISegmentedControl) {
+        NSUserDefaults.standardUserDefaults().setInteger(sender.selectedSegmentIndex, forKey: "initPercentage");
+        
+        NSUserDefaults.standardUserDefaults().synchronize();
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TipCalculatorViewController.swift
+++ TipCalculatorViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-
 class TipCalculatorViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var billAmountTextField: UITextField!
@@ -23,23 +22,39 @@ class TipCalculatorViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "settings", style: UIBarButtonItemStyle.Bordered, target: self, action: "goToSetting");
         tipLabel.text = "$0.00";
         totalLabel.text = "$0.00";
         tooMuchWarningLabel.hidden = true;
         billAmountTextField.delegate = self;
         dogPic.hidden = true;
+        
+        percentSegmentController.selectedSegmentIndex = NSUserDefaults.standardUserDefaults().integerForKey("initPercentage");
 
         // Do any additional setup after loading the view.
     }
     
-    
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated);
+        let defaults = NSUserDefaults.standardUserDefaults();
+        
         billAmountTextField.becomeFirstResponder();
+        let savedDate: NSDate = NSDate(timeIntervalSince1970: defaults.doubleForKey("savedDate"));
+        let diff : NSTimeInterval = NSDate().timeIntervalSinceDate(savedDate);
+        
+        if  diff < 5 * 60 // saved less than 5 mins ago
+        {
+            let oldAmount : Double = defaults.doubleForKey("billAmount");
+            billAmountTextField.text = String(format: "%0.2f", oldAmount);
+            updateTipInfo(oldAmount);
+        }
         
         var tap = UITapGestureRecognizer(target: self, action: Selector("onTap"));
-
         view.addGestureRecognizer(tap);
+        
+        self.navigationController?.navigationBar.barTintColor = UIColor.whiteColor();
+        self.navigationController?.navigationBar.tintColor = UIColor.blueColor();
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -47,6 +62,19 @@ class TipCalculatorViewController: UIViewController, UITextFieldDelegate {
         dogPic.hidden = false;
     }
 
+    
+    func goToSetting()
+    {
+        self.performSegueWithIdentifier("goSetting", sender: self);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -60,7 +88,10 @@ class TipCalculatorViewController: UIViewController, UITextFieldDelegate {
     
     func textField(textField: UITextField!, shouldChangeCharactersInRange range: NSRange, replacementString string: String!) -> Bool
     {
-        let newString = (textField.text as NSString).stringByReplacingCharactersInRange(range, withString: string);
+        var newString = (textField.text as NSString).stringByReplacingCharactersInRange(range, withString: string);
+        
+        newString = getTrimmedString(newString);
+        
         var ret : Bool = countElements(newString.stringByTrimmingCharactersInSet(NSCharacterSet.decimalDigitCharacterSet())) < 2;
         
         if ret{
@@ -71,61 +102,79 @@ class TipCalculatorViewController: UIViewController, UITextFieldDelegate {
             if notOverLimit
             {
                 updateTipInfo(amount);
+                
+                saveBillAmount(amount);
             }
             
             tooMuchWarningLabel.hidden = notOverLimit;
             ret = notOverLimit;
         }
         
+        
+        
         return ret;
     }
     
-//    @IBAction func OnEditingChange(sender: UITextField)
-//    {
-//        let amount = (billAmountTextField.text as NSString).doubleValue;
-//        let notOverLimit : Bool = amount < maxAmount;
-//        
-//        if(notOverLimit)
-//        {
-//            var currentPercentage = (percentSegmentController.titleForSegmentAtIndex(percentSegmentController.selectedSegmentIndex) as NSString).doubleValue * 0.01;
-//            
-//            var tip = amount * currentPercentage;
-//            var total = amount + tip;
-//            
-//            tipLabel.text = String(format: "$%0.2f", tip);
-//            totalLabel.text = String(format:"$%0.2f", total);
-//            currentValue = amount;
-//        }
-//        
-//        billAmountTextField.text = "$\(currentValue)";
-//        tooMuchWarningLabel.hidden = notOverLimit;
-//    }
-
+    
+    func saveBillAmount(amount:Double)
+    {
+        let defaults = NSUserDefaults.standardUserDefaults();
+        
+        defaults.setDouble(amount, forKey: "billAmount");
+        defaults.setObject(NSDate().timeIntervalSince1970, forKey: "savedDate");
+    }
+    
     func updateTipInfo(amount:Double)
     {
-        var currentPercentage = (percentSegmentController.titleForSegmentAtIndex(percentSegmentController.selectedSegmentIndex) as NSString).doubleValue * 0.01;
+        var currentPercentage : Double =  getCurrentPerCentage();
         
         var tip = amount * currentPercentage;
         var total = amount + tip;
         
-        tipLabel.text = String(format: "$%0.2f", tip);
-        totalLabel.text = String(format:"$%0.2f", total);
+        tipLabel.text = getMoneyFormatString(tip);
+        totalLabel.text = getMoneyFormatString(total);
         currentValue = amount;
     }
     
-    
+    func getCurrentPerCentage() -> Double
+    {
+        return (percentSegmentController.titleForSegmentAtIndex(percentSegmentController.selectedSegmentIndex)! as NSString).doubleValue * 0.01
+    }
     
     @IBAction func OnPercentValueChanged(sender: UISegmentedControl) {
         updateTipInfo(currentValue);
     }
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if segue.identifier == "goSetting"
+        {
+            let vc = segue.destinationViewController as SettingsViewController;
+            
+            vc.initialSeletedIndex = percentSegmentController.selectedSegmentIndex;
+        }
     }
-    */
-
+    
+    
+    func getTrimmedString(input : String) -> String
+    {
+//        "^?[0-9]*\\.?[0-9]+$"
+        let match = input.rangeOfString("^?[0-9]*\\.*", options: .RegularExpressionSearch);
+        
+        if (match != nil){
+            return input.substringWithRange(match!);
+        }
+        else{
+            return "";
+        }
+    }
+    
+    
+    func getMoneyFormatString(amount: Double) -> String
+    {
+        return String(format:"$%0.2f", amount);
+    }
+    
 }
